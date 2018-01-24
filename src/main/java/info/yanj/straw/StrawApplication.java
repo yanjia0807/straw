3ba82a8198a4783cb4f5f31e63d45da9e70ec130
yanjia0807 <32187309@qq.com>
2018-01-24 23:46:08 +0800

    init straw code

---
+++ src/main/java/info/yanj/straw/StrawApplication.java
@@ -0,0 +1,12 @@
+package info.yanj.straw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrawApplication.class, args);
+	}
+}
